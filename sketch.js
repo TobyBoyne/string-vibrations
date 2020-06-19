d3eb4df17fbddf00019545e847286c33bfeb89a6
Toby Boyne <tobyboyne@gmail.com>
2020-06-19 14:53:25 +0100

    Adding drawing of sine waves

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 var nodeX = 0,
-	amp = 1,
+	amp = 100,
 	a = 0.5,
 	L = 300,
+	Ns = 3,
 	held = false;
 
 var node1 = {x:50, y:200},
@@ -10,29 +11,40 @@ var node1 = {x:50, y:200},
 
 function setup() {
 	let canvas = createCanvas(400, 400);
-	canvas.parent("stringDiv");
-	const ts = tf.linspace(0, L, 100);
-	var x = getSin(5, ts);
-	console.log(x.array());
+	canvas.parent("stringDiv")
 }
 
 function draw() {
 	background(220);
-	updateLabels(mouseX, mouseY);
+	// updateLabels(mouseX, mouseY);
+	a = (mouseX - node1.x) / L;
+	amp = (mouseY - node1.y);
 	noFill();
 	beginShape();
 	vertex(node1.x, node1.y);
 	vertex(mouseX, mouseY);
 	vertex(node2.x, node2.y);
 	endShape();
+
+	const xs = tf.linspace(0, L, 50);
+	for (n = 1; n < Ns; n++) {
+		let xsArray = xs.dataSync(),
+		 ys = getSin(n, xs).dataSync();
+		beginShape();
+		for (i = 0; i < xs.shape[0]; i++) {
+			console.log(i);
+			vertex(node1.x + xsArray[i], node1.y + ys[i]);
+			console.log(xsArray[i])
+		}
+		endShape()
+	}
 }
 
 
-function getSin(n, ts) {
+function getSin(n, xs) {
 	let a_n = tf.scalar(2 * amp / ((n * PI) ** 2 * a * (1-a)));
-	console.log("a=", a_n);
 	a_n.mul(tf.sin(n * PI * a));
-	let sins = tf.mul(ts, n * PI / L).sin();
+	let sins = tf.mul(xs, n * PI / L).sin();
 	return tf.mul(a_n, sins)
 }
 
