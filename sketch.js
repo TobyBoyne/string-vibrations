b69016ed6a618622015aafb00782fe375ee9f1d3
Toby Boyne <tobyboyne@gmail.com>
2020-06-19 13:09:01 +0100

    Wrote getSin function to generate sine wave for given n

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
 var nodeX = 0,
-	amp = 0,
+	amp = 1,
+	a = 0.5,
+	L = 300,
 	held = false;
 
 var node1 = {x:50, y:200},
-  node2 = {x:350, y:200};
+  node2 = {x:50+L, y:200};
 
 
 function setup() {
 	let canvas = createCanvas(400, 400);
-	canvas.parent("stringDiv")
+	canvas.parent("stringDiv");
+	const ts = tf.linspace(0, L, 100);
+	var x = getSin(5, ts);
+	console.log(x.array());
 }
 
 function draw() {
@@ -20,6 +25,15 @@ function draw() {
 	vertex(mouseX, mouseY);
 	vertex(node2.x, node2.y);
 	endShape();
+}
+
+
+function getSin(n, ts) {
+	let a_n = tf.scalar(2 * amp / ((n * PI) ** 2 * a * (1-a)));
+	console.log("a=", a_n);
+	a_n.mul(tf.sin(n * PI * a));
+	let sins = tf.mul(ts, n * PI / L).sin();
+	return tf.mul(a_n, sins)
 }
 
 function updateLabels(x, y){
