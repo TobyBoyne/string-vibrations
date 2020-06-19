fe025253261822e0a5259ad9e95b7bb3d56d394c
Toby Boyne <tobyboyne@gmail.com>
2020-06-19 15:01:25 +0100

    Mouse now constrained to between nodes

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var nodeX = 0,
 	amp = 100,
 	a = 0.5,
 	L = 300,
-	Ns = 3,
+	Ns = 5,
 	held = false;
 
 var node1 = {x:50, y:200},
@@ -17,24 +17,31 @@ function setup() {
 function draw() {
 	background(220);
 	// updateLabels(mouseX, mouseY);
+
+	const aMax = 0.99,
+		aMin = 0.01;
+
 	a = (mouseX - node1.x) / L;
+	if (a > aMax) {
+		a = aMax
+	} else if (a < aMin) {
+		a = aMin
+	}
 	amp = (mouseY - node1.y);
 	noFill();
 	beginShape();
 	vertex(node1.x, node1.y);
-	vertex(mouseX, mouseY);
+	vertex(node1.x + a * L, mouseY);
 	vertex(node2.x, node2.y);
 	endShape();
 
 	const xs = tf.linspace(0, L, 50);
-	for (n = 1; n < Ns; n++) {
+	for (n = 1; n < Ns + 1; n++) {
 		let xsArray = xs.dataSync(),
 		 ys = getSin(n, xs).dataSync();
 		beginShape();
 		for (i = 0; i < xs.shape[0]; i++) {
-			console.log(i);
 			vertex(node1.x + xsArray[i], node1.y + ys[i]);
-			console.log(xsArray[i])
 		}
 		endShape()
 	}
