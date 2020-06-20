3cab7d59618dfbdcbce764c858394ab13358c0e8
Toby Boyne <tobyboyne@gmail.com>
2020-06-20 23:13:40 +0100

    Improved performance by clearing memory

--- sketch.js
+++ sketch.js
@@ -1,18 +1,21 @@
-var nodeX = 0,
-	amp = 100,
+var amp = 100,
 	a = 0.5,
+	t = 0;
+
+const nodeX = 50,
 	L = 300,
-	Ns = 1,
-	t = 0,
+	aMax = 0.99,
+	aMin = 0.01,
 	timeScale = 2,
-	damping = 0.005,
-	held = false;
+	damping = 0.005;
 
-const aMax = 0.99,
-	aMin = 0.01;
+const node1 = {x:nodeX, y:200},
+  node2 = {x:nodeX+L, y:200};
 
-const node1 = {x:50, y:200},
-  node2 = {x:50+L, y:200};
+const xs = tf.linspace(0, L, 50),
+		xsArray = xs.dataSync();
+
+// var ys = tf.zeros()
 
 const colours = [
 	"#000000",
@@ -28,22 +31,24 @@ const colours = [
 
 function setup() {
 	let canvas = createCanvas(400, 400);
-	canvas.parent("stringDiv")
+	canvas.parent("stringDiv");
+	noFill()
 }
 
 function draw() {
 	background(220);
-	const slider = document.getElementById("numFourier");
-	Ns = parseInt(slider.value, 10);
+	// tf.tidy() deletes tensors from memory after use
+	tf.tidy(() => {
 
+	const slider = document.getElementById("numFourier");
+	let Ns = parseInt(slider.value, 10);
 	// start counting time since string was released
-	held = mouseIsPressed && (mouseButton === LEFT);
+	const held = mouseIsPressed && (mouseButton === LEFT);
 	if (held) {t = 0}
 
 
 	// draw string if being held
 	if (held) {
-		noFill();
 		strokeWeight(4);
 		stroke(colours[0]);
 		beginShape();
@@ -53,15 +58,14 @@ function draw() {
 		endShape();
 	}
 
-	const xs = tf.linspace(0, L, 50),
-		xsArray = xs.dataSync();
+
 	let sinSum = tf.zerosLike(xs);
 
 	// draw each fourier term as a separate sine wave
   // if released, multiply by time dependency
 
 	strokeWeight(2);
-	for (n = 1; n < Ns + 1; n++) {
+	for (let n = 1; n < Ns + 1; n++) {
 		let ys = getSin(n, xs);
 		const freq = 2 * n / L;
 		let timeArray = tf.mul(2 * PI * n * timeScale * t * freq, tf.onesLike(ys));
@@ -85,16 +89,18 @@ function draw() {
 	// plot fourier approximation of curve
 	stroke(colours[1]);
 	beginShape();
-	for (i = 0; i < xs.shape[0]; i++) {
+	for (let i = 0; i < xs.shape[0]; i++) {
 		vertex(node1.x + xsArray[i], node1.y + sinSumArray[i]);
 	}
 	endShape();
 
 	t += 1
+
+	});
 }
 
 
-function getSin(n, xs) {
+function getSin(n) {
 	let a_n = tf.scalar(2 * amp / ((n * PI) ** 2 * a * (1-a)));
 	a_n = a_n.mul(tf.sin(n * PI * a));
 	let sins = tf.mul(xs, n * PI / L).sin();
@@ -103,18 +109,17 @@ function getSin(n, xs) {
 
 function mouseDragged() {
 	// do not update if mouse is outside of canvas, or if not left click
-	if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > width) {return false}
-	if (mouseButton !== LEFT) {return false}
+	if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > width || mouseButton !== LEFT) {}
+	else {
+		a = (mouseX - node1.x) / L;
+		if (a > aMax) {
+			a = aMax
+		} else if (a < aMin) {
+			a = aMin
+		}
 
-	a = (mouseX - node1.x) / L;
-	if (a > aMax) {
-		a = aMax
-	} else if (a < aMin) {
-		a = aMin
+		amp = (mouseY - node1.y);
 	}
-
-	amp = (mouseY - node1.y);
-	return false
 }
 
 
